a3f21837726d310f79fc1c909ac0d6e672b68a72
Jan Hrůza <honza.hruza.d5@seznam.cz>
2025-04-21 23:09:37 +0100

    InfoOverlay: Development progress.

--- InfoOverlay/Header.h
+++ InfoOverlay/Header.h
@@ -1,6 +1,97 @@
 #pragma once
+#include "resource.h"
+#include <stdio.h>
 #include <Windows.h>
 
 #pragma comment(linker,"\"/manifestdependency:type='win32' \
 name='Microsoft.Windows.Common-Controls' version='6.0.0.0' \
 processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
+
+// definitions
+static HWND hCbxWindow;
+static HWND hTxtOverlay;
+
+static BOOL SetOverlayText(_In_ HWND hWnd, _In_ LPWSTR lpszText)
+{
+	if (!hWnd)
+	{
+		return FALSE;
+	}
+
+	if (IsWindowVisible(hWnd) == FALSE)
+	{
+		return FALSE;
+	}
+
+	// get client rect
+	RECT rect;
+	GetClientRect(hWnd, &rect);
+
+	// draw text
+	HDC hdc = GetDC(hWnd);
+	DrawText(hdc, lpszText, lstrlen(lpszText), &rect, DT_RIGHT | DT_TOP);
+	ReleaseDC(hWnd, hdc);
+
+	return TRUE;
+}
+
+static BOOL UnsetOverlayText(_In_ HWND hWnd)
+{
+	if (!hWnd)
+	{
+		return FALSE;
+	}
+
+	if (IsWindowVisible(hWnd) == FALSE)
+	{
+		return FALSE;
+	}
+
+	// get client
+	RECT rect;
+	GetClientRect(hWnd, &rect);
+
+	// remove text
+	HDC hdc = GetDC(hWnd);
+	FillRect(hdc, &rect, (HBRUSH)(COLOR_BACKGROUND + 1));
+	ReleaseDC(hWnd, hdc);
+	return TRUE;
+}
+
+BOOL CALLBACK EnumWindowsProc(HWND hWnd, LPARAM lParam)
+{
+	if (IsWindowVisible(hWnd) == FALSE)
+	{
+		// skip
+		return TRUE;
+	}
+
+	WCHAR wndText[MAX_PATH];
+	GetWindowText(hWnd, wndText, MAX_PATH);
+
+	if (lstrlen(wndText) == 0)
+	{
+		swprintf_s(wndText, MAX_PATH, L"[No Text]\0");
+	}
+
+	const int size = 2 * MAX_PATH + 4;
+	WCHAR wText[2 * MAX_PATH + 4];
+	WCHAR wClass[MAX_PATH];
+
+	RealGetWindowClass(hWnd, wClass, MAX_PATH);
+	swprintf_s(wText, size, L"%s [%s]\0", wndText, wClass);
+
+	int index = SendMessage(hCbxWindow, CB_ADDSTRING, NULL, (LPARAM)wText);
+	SendMessage(hCbxWindow, CB_SETITEMDATA, index, (LPARAM)hWnd);
+	return TRUE;
+}
+
+static void EnumAllWindows(void)
+{
+	if (!hCbxWindow) return;
+
+	SendMessage(hCbxWindow, CB_RESETCONTENT, 0, 0);
+	EnumWindows(EnumWindowsProc, NULL);
+	SendMessage(hCbxWindow, CB_SETCURSEL, 0, 0);
+	return;
+}
--- InfoOverlay/Source.c
+++ InfoOverlay/Source.c
@@ -1,7 +1,67 @@
 #pragma once
 #include "Header.h"
 
+INT_PTR CALLBACK DialogProc(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	switch (message)
+	{
+		case WM_INITDIALOG:
+			hCbxWindow = GetDlgItem(hDlg, IDC_CBX_WINDOW);
+			hTxtOverlay = GetDlgItem(hDlg, IDC_TXT_OVERLAY);
+			return TRUE;
+
+		case WM_COMMAND:
+		{
+			switch (wParam)
+			{
+				// close dialog
+				case IDC_BTN_CANCEL:
+					EndDialog(hDlg, IDCANCEL);
+					return TRUE;
+
+				// refresh windows
+				case IDC_BTN_REFRESH:
+					EnumAllWindows();
+					return TRUE;
+
+				// apply text
+				case IDC_BTN_OK:
+					int index = SendMessage(hCbxWindow, CB_GETCURSEL, 0, 0);
+					HWND hWnd = SendMessage(hCbxWindow, CB_GETITEMDATA, index, 0);
+
+					// check if item has an associated window
+					if (hWnd == CB_ERR) return TRUE;
+
+					// get overlay text
+					WCHAR overlayText[512];
+					GetWindowText(hTxtOverlay, overlayText, 512);
+
+					if (lstrlen(overlayText) == 0)
+					{
+						// remove any overlay
+						UnsetOverlayText(hWnd);
+						return TRUE;
+					}
+
+					// apply overlay text
+					SetOverlayText(hWnd, overlayText);
+					return TRUE;
+			}
+
+			return FALSE;
+		}
+
+		case WM_CLOSE:
+			EndDialog(hDlg, IDCLOSE);
+			return TRUE;
+		}
+
+	return FALSE;
+}
+
 int APIENTRY wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpszCommandLine, int nCmdShow)
 {
+	// main entry point
+	DialogBox(hInstance, MAKEINTRESOURCE(IDD_MAIN_DIALOG), NULL, DialogProc);
 	return 0;
 }
