615ab8721faf16334e65a118f150e4e0d1f7491f
Jan Hrůza <honza.hruza.d5@hotmail.com>
2025-12-12 22:07:52 +0100

    Added a version.

---
+++ CSnake/Source.cpp
@@ -0,0 +1,16 @@
+/*
+* Source.cpp
+* Main entry point.
+*/
+
+#include <Windows.h>
+
+/* App's main entry point. */
+INT WINAPI wWinMain(HINSTANCE hInstance,
+			 HINSTANCE hPrevInstance,
+			 PWSTR lpCommandLine,
+			 int nCmdShow)
+{
+
+	return 0;
+}
