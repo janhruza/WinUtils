8530273480bfc534727a040c1d96974985dc87a2
Jan Hrůza <honza.hruza.d5@hotmail.com>
2026-02-22 02:01:38 +0100

    Added a new update utility: WinUpdate

---
+++ WinUpdate/Source.c
@@ -0,0 +1,96 @@
+#pragma once
+#include "inc/WindowsUpdate.h"
+
+inline static void flushstdin()
+{
+	int c;
+	while ((c = getchar()) != '\n' && c != EOF) {}
+	return;
+}
+
+
+
+INT wmain(int argc, const wchar_t* argv[])
+{
+	if (argc > 1)
+	{
+		fprintf_s(stderr, "Arguments are disabled.\n");
+		return EXIT_FAILURE;
+	}
+
+	SetConsoleTitle(TEXT("WinUpdate"));
+
+	for (;;)
+	{
+		printf_s("1. Check for updates\n");
+		printf_s("2. Install pending updates\n");
+		printf_s("3. Install new package(s)\n");
+		printf_s("4. Remove package(s)\n");
+		printf_s("0. Exit\n\n");
+
+		int option = 0xFF; // must be initialized to prevent errors and invalid options
+		printf_s("Enter option [0-4]: ");
+		scanf_s("%d", &option);
+		flushstdin();
+
+		switch (option)
+		{
+		case 1:
+			// check for updates
+			printf_s("\n");
+			WuCheckForUpdates();
+			printf_s("\n");
+			break;
+
+		case 2:
+			// instal all pending updates
+			printf_s("\n");
+			WuInstallUpdates();
+			printf_s("\n");
+			break;
+
+		case 3:
+			// install new package(s)
+			{
+				printf_s("\n");
+				CHAR packageIds[1024] = { 0 };
+				printf_s("List all package id's separated by spaces\n# ");
+
+				if (fgets(packageIds, sizeof(packageIds), stdin) != NULL)
+				{
+					WuInstallPackages(packageIds);
+				}
+
+				printf_s("\n");
+			}
+			break;
+
+		case 4:
+			// remove package(s)
+			{
+				printf_s("\n");
+				CHAR packageIds[1024] = { 0 };
+				printf_s("List all package id's separated by spaces\n# ");
+
+				if (fgets(packageIds, sizeof(packageIds), stdin) != NULL)
+				{
+					WuRemovePackages(packageIds);
+				}
+
+				printf_s("\n");
+			}
+			break;
+
+		case 0:
+			// break from the loop and exit the program
+			goto processExit;
+
+		default:
+			// option not found
+			break;
+		}
+	}
+
+	processExit:
+	return EXIT_SUCCESS;
+}
---
+++ WinUpdate/inc/WindowsUpdate.h
@@ -0,0 +1,53 @@
+#pragma once
+#include <stdio.h>
+#include <Windows.h>
+#include <string.h>
+
+static inline BOOL WuStartProcess(LPCSTR program, LPCSTR args)
+{
+	LPSTR command[80];
+	sprintf_s(command, 80, "%s %s\0", program, args);
+	return system(command);
+}
+
+/// <summary>
+/// Checks for available updates.
+/// </summary>
+/// <returns>Operation result.</returns>
+inline static BOOL WuCheckForUpdates(void)
+{
+	return WuStartProcess("winget", "update");
+}
+
+/// <summary>
+/// Runs the upgrade process for all upgradable packages.
+/// </summary>
+/// <returns>Operation result.</returns>
+inline static BOOL WuInstallUpdates(void)
+{
+	return WuStartProcess("winget", "update --all");
+}
+
+/// <summary>
+/// Installs all given packages.
+/// </summary>
+/// <param name="packageIds">List of all packages to install. These values must be space-separated.</param>
+/// <returns>Operation result.</returns>
+inline static BOOL WuInstallPackages(LPCSTR packageIds)
+{
+	CHAR args[1024] = { 0 };
+	sprintf_s(args, sizeof(args), "install %s", packageIds);
+	return WuStartProcess("winget", args);
+}
+
+/// <summary>
+/// Removes all given packages.
+/// </summary>
+/// <param name="packageIds">List of all packages to remove. These values must be space-separated.</param>
+/// <returns>Operation result.</returns>
+inline static BOOL WuRemovePackages(LPCSTR packageIds)
+{
+	CHAR args[1024] = { 0 };
+	sprintf_s(args, sizeof(args), "remove %s", packageIds);
+	return WuStartProcess("winget", args);
+}
